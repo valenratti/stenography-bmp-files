d40383ef56af916f870bc5e169214090a6a7042b
lgalende <lgalende@itba.edu.ar>
2022-06-17 01:07:44 -0300

    cipher

--- input_options.h
+++ input_options.h
@@ -2,13 +2,13 @@
 #define INPUT_OPTIONS_H
 
 struct input_options {
-    char* in; // file to be hidden
-    char* p; // bmp file which will be used as carrier
-    char* out; // name of the final bmp file
-    char* steg; // estenography algorithm used
-    char *a; // cypher algorithm used
-    char *m; // cypher mode
-    char *pass; // encryption password
+    char *in;       // file to be hidden
+    char *p;        // bmp file which will be used as carrier
+    char *out;      // name of the final bmp file
+    char *steg;     // stenography algorithm used
+    char *a;        // cypher algorithm used
+    char *m;        // cypher mode
+    char *pass;     // encryption password
 };
 
 #endif
---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+
+#include "input_options.h"
+
+int main() {
+    // TODO: Parse arguments
+
+    struct input_options *opt = malloc(struct *input_options);
+
+    opt->a = "aes128";
+    opt->m = "cbc";
+    opt->pass = "lautaro";
+
+    // encrypt
+    // print
+    // decrypt
+    // print
+
+    return 0;
+}
